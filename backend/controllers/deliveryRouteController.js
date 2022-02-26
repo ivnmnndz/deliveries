222b2cef5f71a1b6b66083cf5bf8cd1517e6383d
IvanM <iamnndz@gmail.com>
2022-02-26 12:43:02 -0500

    inital REST API

--- backend/controllers/deliveryRouteController.js
+++ backend/controllers/deliveryRouteController.js
@@ -1,33 +1,56 @@
+const DeliveryRoute = require("../models/deliveryModel");
+
 //@desc Get delivery routes
 //@route GET /api/routes
 //@access private
-const getDeliveryRoutes = (req, res) => {
-  res.status(200).json({ routes: "get delivery routes" });
+const getDeliveryRoutes = async (req, res) => {
+  const deliveryRoutes = await DeliveryRoute.find();
+
+  res.status(200).json(deliveryRoutes);
 };
 
 //@desc Set delivery route
 //@route POST /api/routes
 //@access private
-const addDeliveryRoute = (req, res) => {
+const addDeliveryRoute = async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add text field");
   }
-  res.status(200).json({ routes: "set delivery routes" });
+  const deliveryRoute = await DeliveryRoute.create({
+    text: req.body.text,
+  });
+  res.status(200).json(deliveryRoute);
 };
 
 //@desc Update delivery routes
 //@route PUT /api/routes/:id
 //@access private
-const updateDeliveryRoute = (req, res) => {
-  res.status(200).json({ routes: `update delivery route ${req.params.id}` });
+const updateDeliveryRoute = async (req, res) => {
+  const deliveryRoute = await DeliveryRoute.findById(req.params.id);
+  if (!deliveryRoute) {
+    res.status(400);
+    throw new Error("Delivery route not found");
+  }
+  const updatedDeliveryRoute = await DeliveryRoute.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedDeliveryRoute);
 };
 
 //@desc Delete delivery routes
 //@route DELETE /api/routes/:id
 //@access private
-const deleteDeliveryRoute = (req, res) => {
-  res.status(200).json({ routes: `delete delivery route ${req.params.id}` });
+const deleteDeliveryRoute = async (req, res) => {
+  const deliveryRoute = await DeliveryRoute.findById(req.params.id);
+  if (!deliveryRoute) {
+    res.status(400);
+    throw new Error("Delivery route not found");
+  }
+  await deliveryRoute.remove();
+  res.status(200).json({ id: req.params.id });
 };
 
 module.exports = {
